8426995357f11103c939ed110d772a9e3fb42b96
Teodelas <teodelas@outlook.com>
2024-04-14 11:58:47 -0400

    Update get-alerts.sh

--- get-alerts.sh
+++ get-alerts.sh
@@ -1,7 +1,10 @@
-#Update with your API endpoint, access key and secret
+#Update variables
 PC_APIURL="https://apix.prismacloud.io"
 PC_ACCESSKEY=""
 PC_SECRETKEY=""
+REPORT_DATE=$(date  +%m_%d_%y)
+REPORT_LOCATION="$HOME/reports/alert_report_$REPORT_DATE.csv"
+mkdir -p $HOME/reports
 
 AUTH_PAYLOAD=$(cat <<EOF
 {"username": "$PC_ACCESSKEY", "password": "$PC_SECRETKEY"}
@@ -17,8 +20,11 @@ PC_JWT_RESPONSE=$(curl -s --request POST \
 
 PC_JWT=$(printf %s "$PC_JWT_RESPONSE" | jq -r '.token' )
 
+echo "Id, Policy Name, Policy Type, Severity, Resource Name, Cloud Type, Account Id, Resource Id" > $REPORT_LOCATION
+
+
 curl -L -X GET \
         --url "$PC_APIURL/v2/alert?alert.status=open&detailed=true&fields=alert.id,policy.name,policy.type,policy.label,policy.severity,resource.name,cloud.type,cloud.account,cloud.accountId,resource.id,alert.status" \
         -H 'Accept: */*' \
         -H 'Content-Type: application/json; charset=UTF-8' \
-        -H "x-redlock-auth: $PC_JWT" | jq .items[] | jq -r '[.id, .policy.name, .policy.policyType, .policy.severity, .resource.name, .resource.cloudType, .resource.accountId, .resource.id] | @csv' > output.csv
+        -H "x-redlock-auth: $PC_JWT" | jq .items[] | jq -r '[.id, .policy.name, .policy.policyType, .policy.severity, .resource.name, .resource.cloudType, .resource.accountId, .resource.id] | @csv' >> $REPORT_LOCATION
