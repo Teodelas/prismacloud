4faea2c9e8e5b12ed65c27ec8d02acb206c82106
Teodelas <teodelas@outlook.com>
2024-04-17 13:53:13 -0400

    Update get-alerts.sh

--- get-alerts.sh
+++ get-alerts.sh
@@ -20,11 +20,11 @@ PC_JWT_RESPONSE=$(curl -s --request POST \
 
 PC_JWT=$(printf %s "$PC_JWT_RESPONSE" | jq -r '.token' )
 
-echo "Id, Policy Name, Policy Type, Severity, Resource Name, Cloud Type, Account Id, Resource Id" > $REPORT_LOCATION
-
+echo "Cloud Account Id, Account Name, Cloud and Region, Resource Type, Resource Name, Alert on Policy, Description, Policy Severity, Recommendation, Status" > $REPORT_LOCATION
 
 curl -L -X GET \
-        --url "$PC_APIURL/v2/alert?alert.status=open&detailed=true&fields=alert.id,policy.name,policy.type,policy.label,policy.severity,resource.name,cloud.type,cloud.account,cloud.accountId,resource.id,alert.status" \
+        --url "$PC_APIURL/v2/alert?alert.status=open&detailed=true" \
         -H 'Accept: */*' \
         -H 'Content-Type: application/json; charset=UTF-8' \
-        -H "x-redlock-auth: $PC_JWT" | jq .items[] | jq -r '[.id, .policy.name, .policy.policyType, .policy.severity, .resource.name, .resource.cloudType, .resource.accountId, .resource.id] | @csv' >> $REPORT_LOCATION
+        -H "x-redlock-auth: $PC_JWT" | jq .items[] | jq -r '[.resource.accountId, .resource.account, .resource.region, .resource.resourceType, .resource.name, .policy.name, .policy.description, .policy.severity, .policy.recommendation, .status] | @csv' >> $REPORT_LOCATION
+
