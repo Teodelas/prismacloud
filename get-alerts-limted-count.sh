a70c35857b2d09c00368598d56a240c4d8e5d95f
Teodelas <teodelas@outlook.com>
2024-04-19 10:55:03 -0400

    Update get-alerts-limted-count.sh

--- get-alerts-limted-count.sh
+++ get-alerts-limted-count.sh
@@ -27,4 +27,5 @@ curl -L -X GET \
         --url "$PC_APIURL/v2/alert?alert.status=open&detailed=true&limit=100" \
         -H 'Accept: */*' \
         -H 'Content-Type: application/json; charset=UTF-8' \
-        -H "x-redlock-auth: $PC_JWT" | jq .items[] | jq -r '[.resource.accountId, .resource.account, .resource.region, .resource.resourceType, .resource.name, .policy.name, .policy.description, .policy.severity, .policy.recommendation, .status] | @csv' >> $REPORT_LOCATION
+        -H "x-redlock-auth: $PC_JWT" | jq ' .items |  group_by(.resource.name)[] | .[]' | jq -r '[.resource.accountId, .resource.account, .resource.region, .resource.resourceType, .resource.name, .policy.name, .policy.description, .policy.severity, .policy.recommendation, .status] | @csv' >> $REPORT_LOCATION
+        
